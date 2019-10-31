632d956b6399c7f98938f44be391819d0172e7a4
Nathan <njmeyer99@gmail.com>
2019-10-31 15:26:46 +1100

    Finished Unity section

---
+++ ComputerGraphics_InEngineApplication/Assets/Scripts/Cube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Red material for the cube
+    public Material redMat = null;
+    // Green material for the cube
+    public Material greenMat = null;
+
+    // Keeps track of if the colour (red) is its base colour or the changed colour (green)
+    bool beenChanged = false;
+
+    // Keeps track of the cubes current material
+    private Material currentCubeMaterial = null;
+
+    private void Start()
+    {
+        // Set the material to red when the app starts
+        GetComponent<MeshRenderer>().material = redMat;
+    }
+
+    private void Update()
+    {
+        // Check the material that is currently on the Cube
+        currentCubeMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    public void ChangeColour()
+    {
+        // If the cube is currently red
+         if (currentCubeMaterial != greenMat && beenChanged == false)
+         {
+            // Change the colour of the cube to green
+            GetComponent<MeshRenderer>().material = greenMat;
+            beenChanged = true;
+         }
+         // If the cube is currently green
+        else if (currentCubeMaterial != redMat && beenChanged == true)
+        {
+            // Change the colour of the cube to red
+            GetComponent<MeshRenderer>().material = redMat;
+            beenChanged = false;
+        }
+    }
+}
--- ComputerGraphics_InEngineApplication/Assets/Scripts/Player.cs
+++ ComputerGraphics_InEngineApplication/Assets/Scripts/Player.cs
@@ -3,45 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof(CharacterController))]
+[RequireComponent (typeof(Animator))]
 public class Player : MonoBehaviour
 {
     CharacterController controller = null;
     Animator animator = null;
 
+    // Speed the character moves
     public float speed = 80.0f;
+    // Amount of force the character exerts on the other objects
     public float pushPower = 2.0f;
+
+    // Prefab for the spheres that spawn
+    public GameObject spherePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Gets the character controller from the object
         controller = GetComponent<CharacterController>();
+        // Gets the character controller from the object
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Allows the character to be interacted with
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-        controller.SimpleMove(transform.forward * vertical * speed * Time.deltaTime);
+        // Moves the charater
+        controller.SimpleMove(transform.up * Time.deltaTime);
         transform.Rotate(transform.up, horizontal * speed * Time.deltaTime);
+        // Plays the animation when the character moves
+        animator.SetFloat("Speed", vertical * speed * Time.deltaTime);
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        // Get the rigidbody of the object the character collided with
         Rigidbody body = hit.collider.attachedRigidbody;
+
+        // If it doesn't have a rigidbody or is kinematic ignore it
         if (body == null || body.isKinematic)
             return;
+        // Don't move the object if there is not enough force
         if (hit.moveDirection.y < 0.3f)
             return;
 
+        // Calculate the direction the object will move when pushed
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        // Push the other object
         body.velocity = pushDir * pushPower;
     }
 
-    void SpawnCube()
+    public void SpawnSpheres()
     {
-
-    }
+        // Spawns a sphere
+        Instantiate(spherePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 2), Quaternion.identity);
+    }   
 }
